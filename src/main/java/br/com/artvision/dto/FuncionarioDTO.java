3f3ee1c0407f55a653a707f3a09434338bce14f6
ERICK SANTOS DA SILVA <erick.ssilva19@senacsp.edu.br>
2025-05-16 20:43:05 -0300

    Subindo classe FuncionarioDTO

---
+++ src/main/java/br/com/artvision/dto/FuncionarioDTO.java
@@ -0,0 +1,80 @@
+package br.com.artvision.dto;
+
+public class FuncionarioDTO {
+    private int idFunc;
+    private String nomeFunc;
+    private String telefoneFunc;
+    private String emailFunc;
+    private int idCargo;
+    private int idSetor;
+    private int idDepto;
+
+    public FuncionarioDTO() {
+    }
+
+    public FuncionarioDTO(int idFunc, String nomeFunc, String telefoneFunc, String emailFunc, int idCargo, int idSetor, int idDepto) {
+        this.idFunc = idFunc;
+        this.nomeFunc = nomeFunc;
+        this.telefoneFunc = telefoneFunc;
+        this.emailFunc = emailFunc;
+        this.idCargo = idCargo;
+        this.idSetor = idSetor;
+        this.idDepto = idDepto;
+    }
+
+    public int getIdFunc() {
+        return idFunc;
+    }
+
+    public void setIdFunc(int idFunc) {
+        this.idFunc = idFunc;
+    }
+
+    public String getNomeFunc() {
+        return nomeFunc;
+    }
+
+    public void setNomeFunc(String nomeFunc) {
+        this.nomeFunc = nomeFunc;
+    }
+
+    public String getTelefoneFunc() {
+        return telefoneFunc;
+    }
+
+    public void setTelefoneFunc(String telefoneFunc) {
+        this.telefoneFunc = telefoneFunc;
+    }
+
+    public String getEmailFunc() {
+        return emailFunc;
+    }
+
+    public void setEmailFunc(String emailFunc) {
+        this.emailFunc = emailFunc;
+    }
+
+    public int getIdCargo() {
+        return idCargo;
+    }
+
+    public void setIdCargo(int idCargo) {
+        this.idCargo = idCargo;
+    }
+
+    public int getIdSetor() {
+        return idSetor;
+    }
+
+    public void setIdSetor(int idSetor) {
+        this.idSetor = idSetor;
+    }
+
+    public int getIdDepto() {
+        return idDepto;
+    }
+
+    public void setIdDepto(int idDepto) {
+        this.idDepto = idDepto;
+    }
+}
