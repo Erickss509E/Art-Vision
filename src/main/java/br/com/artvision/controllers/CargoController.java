a793ba1482b3db2e3a17c94be822363e96070f0b
Murile <murilobalves04@gmail.com>
2025-05-19 23:19:37 -0300

    Restruturação de pastas do projeto

--- src/main/java/br/com/artvision/controllers/CargoController.java
+++ src/main/java/br/com/artvision/controllers/CargoController.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/cargo")
+@WebServlet("/sistema/cargo")
 public class CargoController extends HttpServlet {
 
     private CargoService cargoService = new CargoService();
